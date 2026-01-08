38473b08df166f4bebfe36220fba53a0b781f7db
Synbi <107513274+Synby@users.noreply.github.com>
2026-01-08 04:35:48 -0600

    add new stuff :3

--- Containerfile
+++ Containerfile
@@ -2,14 +2,14 @@ FROM scratch as ctx
 
 COPY build_files /build
 COPY system_files /files
-COPY cosign.pub /files/etc/pki/containers/bootc-template.pub
+COPY cosign.pub /files/etc/pki/containers/zirc-addons.pub
 
 # base image - replace with the image url of whatever you want to build from
 # other options for a baseimage are:
 #    ghcr.io/ublue-os/aurora:stable
 #    quay.io/fedora/fedora-bootc:43
 #    quay.io/fedora-ostree-desktops/silverblue:43
-FROM ghcr.io/ublue-os/bluefin:stable
+FROM ghcr.io/zirconium-dev/zirconium:latest
 
 ### [IM]MUTABLE /opt
 ## Some bootable images, like Fedora, have /opt symlinked to /var/opt, in order to
--- build_files/build.sh
+++ build_files/build.sh
@@ -5,9 +5,7 @@ set -eoux pipefail
 # copy over any added system files to the new image
 cp -avf "/ctx/files"/. /
 
-# install packages or run commands to edit image properties, go wild
-# just be careful with installing nvidia drivers, those are a PITA
-# for those, i'd recommend copying what zirconium does
+## installing trivalent
 
 # trivalent
 dnf -y config-manager addrepo --from-repofile=https://repo.secureblue.dev/secureblue.repo
@@ -23,3 +21,45 @@ dnf -y install --enablerepo copr:copr.fedorainfracloud.org:secureblue:trivalent 
 
 # trivalent expects this file to exist because hardened-malloc would generally create it
 touch /etc/ld.so.preload
+
+## HWE features 
+
+# remove old kernel
+for pkg in kernel kernel-core kernel-modules kernel-modules-core; do
+  rpm --erase $pkg --nodeps
+done
+
+# disable initramfs generation
+ln -sf /bin/true /usr/lib/kernel/install.d/05-rpmostree.install
+ln -sf /bin/true /usr/lib/kernel/install.d/50-dracut.install
+
+# install cachy kernel
+dnf -y copr enable bieszczaders/kernel-cachyos
+dnf -y copr disable bieszczaders/kernel-cachyos
+dnf -y --enablerepo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos install \
+  kernel-cachyos \
+  kernel-cachyos-devel-matched
+
+# install addons
+dnf -y copr enable bieszczaders/kernel-cachyos-addons
+dnf -y copr disable bieszczaders/kernel-cachyos-addons
+dnf -y --enablerepo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-addons swap \
+  zram-generator-defaults \
+  cachyos-settings
+dnf -y --enablerepo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-addons install \
+  scx-scheds-git \
+  scx-manager
+
+# asus linux packages
+dnf -y copr enable lukenukem/asus-linux
+dnf -y copr disable lukenukem/asus-linux
+dnf -y --enablerepo copr:copr.fedorainfracloud.org:lukenukem/asus-linux install \
+  asusctl \
+  asusctl-rog-gui \
+  supergfxctl
+systemctl enable supergfxd.service
+
+# kargs
+tee /usr/lib/bootc/kargs.d/01-HWE.toml << 'EOF'
+kargs = ["amdgpu.dcdebugmask=0x10"]
+EOF
