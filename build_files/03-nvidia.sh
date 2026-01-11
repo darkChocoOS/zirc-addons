eb0cc7094c9515600ca3f3dd5027a4c83e8a5b31
Synbi <107513274+Synby@users.noreply.github.com>
2026-01-11 00:08:53 -0600

    fix

--- build_files/03-nvidia.sh
+++ build_files/03-nvidia.sh
@@ -6,7 +6,7 @@ fi
 
 set -xeuo pipefail
 
-KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/modules" -exec basename '{}' ';' | sort | tail -n 1)"
+KERNEL_VERSION="$(ls /usr/lib/modules | head -n1)"
 
 dnf config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-nvidia.repo
 dnf config-manager setopt fedora-nvidia.enabled=0
--- build_files/99-cleanup.sh
+++ build_files/99-cleanup.sh
@@ -3,9 +3,8 @@
 # save space
 rm -rf /usr/share/doc
 rm -rf /usr/bin/chsh
-
-KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/modules" -exec basename '{}' ';' | sort | tail -n 1)"
-echo $KERNEL_VERSION
+ 
+KERNEL_VERSION="$(ls /usr/lib/modules | head -n1)"
 export DRACUT_NO_XATTR=1
 dracut --no-hostonly --kver "$KERNEL_VERSION" --reproducible --zstd -v --omit pcsc --add ostree -f "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
 chmod 0600 "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
