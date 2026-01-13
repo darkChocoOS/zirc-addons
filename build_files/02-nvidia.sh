279056e5115788c4ea609c500138a9bafdf21393
Synbi <107513274+Synby@users.noreply.github.com>
2026-01-13 00:58:31 -0600

    Revert "fix"
    This reverts commit eb0cc7094c9515600ca3f3dd5027a4c83e8a5b31.

--- build_files/02-nvidia.sh
+++ build_files/02-nvidia.sh
@@ -2,7 +2,7 @@
 
 set -xeuo pipefail
 
-KERNEL_VERSION="$(ls /usr/lib/modules | head -n1)"
+KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/modules" -exec basename '{}' ';' | sort | tail -n 1)"
 
 dnf config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-nvidia.repo
 dnf config-manager setopt fedora-nvidia.enabled=0
